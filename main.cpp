7bffdaf981a1b5ea0ba40883082bac5df1ee303c
Anomalybay <sheributt094@gmail.com>
2025-07-23 11:00:29 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+    return 0;
+}
